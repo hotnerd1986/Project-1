13b437888c9c7ab3b198c0e83d9d869890407ed2
Jordache1986 <jordache.uct@gmail.com>
2018-03-16 10:08:46 +0200

    Initial commit

---
+++ R/age_function.R
@@ -0,0 +1,11 @@
+#' age function
+#'
+#' @details Some function created
+#' @return age (integer)
+#' @export
+#'
+#' @examples
+create_age<-function(){
+  age<-sample(1:100,size=1)
+  return(age)
+}
